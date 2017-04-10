56f253402b79d50ab48bc468448ac67290502e03
Daniel Jiang <daniel.lu.jiang@gmail.com>
2017-04-09 22:50:14 -0700

    Move top level code into a main function

--- main.py
+++ main.py
@@ -1,32 +1,107 @@
+import argparse
 import csv
+
 import numpy as np
+
 import data
 from utils import *
 
-all_data = data.get()
-all_data = None
-with open('/Users/daniel/Developer/ICME/data', 'r') as f:
-    all_data = eval(f.read())
+def main(MAX_ITERATIONS=0):
+    #all_data = data.get()
+    all_data = None
+    with open('/Users/daniel/Developer/ICME/data', 'r') as f:
+        all_data = eval(f.read())
 
-all_data = data.filter(all_data, 'ca')
-print(len(all_data))
+    all_data = data.filter(all_data, 'ca')
+    print(len(all_data))
 
-'''
-# Basic four node test
-all_data = []
-all_data.append({'watchlink': 0, 'latitude': 80, 'longitude': 80, 'keywords': 'berkeley'})
-all_data.append({'watchlink': 1, 'latitude': 80, 'longitude': 80, 'keywords': 'berkeley, campanile'})
-all_data.append({'watchlink': 2, 'latitude': 80, 'longitude': 80, 'keywords': 'haas, campanile'})
-all_data.append({'watchlink': 3, 'latitude': 90, 'longitude': 80, 'keywords': 'haas'})
-'''
-train_data, test_data = data.split(all_data, 0.8)
+    train_data, test_data = data.split(all_data, 0.8)
 
-MAX_ITERATIONS = 0
-CONVERGENCE_THRESHOLD = 0.00006288 # About the mean sqaured difference of 1km
-DROP_EDGE_THRESHOLD = len(all_data) * 0.1
-LOCALITY_THRESHOLD = 1 # Locality of 'newyorkcity' is 0.057
-img_data_mappings = {}
-G = UndirectedGraph()
+    CONVERGENCE_THRESHOLD = 0.00006288 # About the mean sqaured difference of 1km
+    LOCALITY_THRESHOLD = 1 # Locality of 'newyorkcity' is 0.057
+    img_data_mappings = {}
+    G = UndirectedGraph()
+
+    locality = get_tag_locality()
+
+    tag_approx_loc, train_tag_counts = process_train_tags(train_data, locality, LOCALITY_THRESHOLD)
+    tag_to_imgs = process_training_data(train_data, img_data_mappings, tag_approx_loc, train_tag_counts)
+
+    total_tag_counts = process_test_data(test_data, train_data, train_tag_counts, img_data_mappings, tag_approx_loc, tag_to_imgs, locality, LOCALITY_THRESHOLD)
+    # Add vertices to graph
+    for img_id in img_data_mappings:
+        G.add_vertex(img_id)
+
+
+    # Add edges to graph
+    edge_count = 0
+
+    for tag in tag_to_imgs:
+        neighbors = tag_to_imgs[tag]
+        for i in range(len(neighbors) - 1):
+            for j in range(i + 1, len(neighbors)):
+                edge_count += 1
+                G.add_edge(neighbors[i], neighbors[j])
+                
+
+    print('Num edges: ' + str(edge_count))
+
+    # Perform location estimate update algorithm
+    mean_squared_change = 100 # Arbitrary number above CONVERGENCE_THRESHOLD
+    num_iter = 0
+    has_converged = True
+    while mean_squared_change > CONVERGENCE_THRESHOLD:
+        if num_iter >= MAX_ITERATIONS:
+            has_converged = False
+            break
+        num_iter += 1
+
+        new_img_data_mappings = img_data_mappings.copy()
+        mean_squared_change = 0
+        for iteration, test_img in enumerate(test_data):
+            img_id = test_img['watchlink']
+            loc = img_data_mappings[img_id]
+            lat, lon, var, delta_squared = calc_update(img_id, loc, G, img_data_mappings)
+            new_img_data_mappings[img_id] = Location(lat, lon, var)
+            mean_squared_change = mean_squared_change / (iteration+1) * iteration + (delta_squared / (iteration + 1))
+        img_data_mappings = new_img_data_mappings
+    print('Num iterations: {0}'.format(num_iter))
+    print('Converged?: {0}'.format(has_converged))
+    # Calculate error
+    median_error_finder = MedianFinder()
+    one_km_count = 0
+    five_km_count = 0
+    ten_km_count = 0
+    hundred_km_count = 0
+    thousand_km_count = 0
+    other_count = 0
+    for test_img in test_data:
+        img_id = test_img['watchlink']
+        img_result_loc = img_data_mappings[img_id]
+        img_actual_loc = Location(float(test_img['latitude']), float(test_img['longitude']))
+        error = Location.dist(img_result_loc, img_actual_loc)
+        median_error_finder.add(error)
+        if error < 1:
+            one_km_count += 1
+            print(img_id + ': ' + repr(img_result_loc))
+        elif error < 5:
+            five_km_count += 1
+        elif error < 10:
+            ten_km_count += 1
+        elif error < 100:
+            hundred_km_count += 1
+        elif error < 1000:
+            thousand_km_count += 1
+        else:
+            other_count += 1
+    print('Less than 1 km: {0}'.format(one_km_count))
+    print('Less than 5 km: {0}'.format(five_km_count))
+    print('Less than 10 km: {0}'.format(ten_km_count))
+    print('Less than 100 km: {0}'.format(hundred_km_count))
+    print('Less than 1000 km: {0}'.format(thousand_km_count))
+    print('Greater than 1000 km: {0}'.format(other_count))
+    print('Med error: {0}'.format(median_error_finder.med()))
+
 
 
 def get_tag_locality():
@@ -35,9 +110,8 @@ def get_tag_locality():
         locality_str = f.read()
     return eval(locality_str)
 
-locality = get_tag_locality()
 
-def process_train_tags(train_data, locality):
+def process_train_tags(train_data, locality, locality_threshold):
     def get_avg(lst):
         if len(lst) == 0:
             return 0
@@ -57,7 +131,7 @@ def process_train_tags(train_data, locality):
         lat, lon = float(train_img['latitude']), float(train_img['longitude'])
         img_tags = train_img['tags']
 
-        remove_low_locality_tags(img_tags)
+        remove_low_locality_tags(locality, img_tags, locality_threshold)
 
         # Gather locations for each tag
         for tag in img_tags:
@@ -90,7 +164,8 @@ def process_train_tags(train_data, locality):
                 tag_mean_loc[tag] = Location(avg_lat, avg_lon, var)
     return tag_mean_loc, train_tag_counts
 
-def remove_low_locality_tags(tags_list):
+
+def remove_low_locality_tags(locality, tags_list, threshold):
     '''Drop low locality tags. Mutates original list'''
     tags_to_remove = []
     for tag in tags_list:
@@ -100,14 +175,10 @@ def remove_low_locality_tags(tags_list):
             locality_score = locality[tag]
             if type(locality_score) is tuple:
                 locality_score = locality_score[0]
-            if locality_score < LOCALITY_THRESHOLD: 
+            if locality_score < threshold: 
                 tags_to_remove.append(tag)
     for tag in tags_to_remove:
         tags_list.remove(tag)
-
-   
-
-tag_approx_loc, train_tag_counts = process_train_tags(train_data, locality)
 
 
 def process_training_data(train_data, img_data_mappings, tag_approx_loc, train_tag_counts):
@@ -130,11 +201,9 @@ def process_training_data(train_data, img_data_mappings, tag_approx_loc, train_t
 
         img_data_mappings[img_id] = Location(lat, lon, min_var)
     return tag_to_imgs
-    
+ 
 
-tag_to_imgs = process_training_data(train_data, img_data_mappings, tag_approx_loc, train_tag_counts)
-
-def process_test_data(test_data, train_tag_counts, img_data_mappings, tag_approx_loc, tag_to_imgs):
+def process_test_data(test_data, train_data, train_tag_counts, img_data_mappings, tag_approx_loc, tag_to_imgs, locality, locality_threshold):
     total_tag_counts = train_tag_counts.copy()
     
     # Process test data
@@ -142,7 +211,7 @@ def process_test_data(test_data, train_tag_counts, img_data_mappings, tag_approx
         img_id = test_img['watchlink']
         img_tags = test_img['tags']
 
-        remove_low_locality_tags(img_tags)
+        remove_low_locality_tags(locality, img_tags, locality_threshold)
 
         # Count tags
         total_tag_counts.add_counts(img_tags)
@@ -163,54 +232,9 @@ def process_test_data(test_data, train_tag_counts, img_data_mappings, tag_approx
         img_data_mappings[img_id] = min_var_loc
     return total_tag_counts
 
-total_tag_counts = process_test_data(test_data, train_tag_counts, img_data_mappings, tag_approx_loc, tag_to_imgs)
 
-# Add vertices to graph
-for img_id in img_data_mappings:
-    G.add_vertex(img_id)
+def calc_update(img, loc, G, img_data_mappings):
 
-
-# Add edges to graph
-edge_count = 0
-
-for tag in tag_to_imgs:
-    neighbors = tag_to_imgs[tag]
-    for i in range(len(neighbors) - 1):
-        for j in range(i + 1, len(neighbors)):
-            edge_count += 1
-            G.add_edge(neighbors[i], neighbors[j])
-                
-
-print('Num edges: ' + str(edge_count))
-
-###################
-'''
-G = UndirectedGraph()
-G.add_vertex('berkeley')
-G.add_vertex('sathergate')
-G.add_vertex('iphone')
-G.add_vertex('test')
-G.add_edge('iphone', 'test')
-G.add_edge('berkeley', 'test')
-G.add_edge('sathergate', 'test')
-img_data_mappings = {}
-img_data_mappings['iphone'] = Location(36.02264117460317,-36.72232646031748,15098163.310764369)
-img_data_mappings['berkeley'] = Location(80, 80, .00001)
-img_data_mappings['sathergate'] = Location(90, 90, .0000001)
-img_data_mappings['test'] = Location(90, 90, .01)
-test_data = []
-test_data.append({'watchlink': 'test', 'latitude': '90', 'longitude': '90'})
-'''
-###################
-
-def calc_update(img, loc, G):
-    '''
-    if img == 'http://flickr.com/photos/32409718@N00/4189020951':
-        for neighbor in G.neighbors(img):
-            print(neighbor + ': ' + repr(img_data_mappings[neighbor]))
-    print()
-    print()
-    '''
     def safe_div(num1, num2):
         if num2 == 0:
             return num1 / (10 ** -40)
@@ -253,78 +277,14 @@ def calc_update(img, loc, G):
     delta_squared = ((loc.lat - mean[0])**2 + (loc.lon - mean[1])**2)/2
     return mean[0], mean[1], var, delta_squared
 
-# Perform location estimate update algorithm
-mean_squared_change = 100 # Arbitrary number above CONVERGENCE_THRESHOLD
-num_iter = 0
-has_converged = True
-while mean_squared_change > CONVERGENCE_THRESHOLD:
-    if num_iter >= MAX_ITERATIONS:
-        has_converged = False
-        break
-    num_iter += 1
 
-    new_img_data_mappings = img_data_mappings.copy()
-    mean_squared_change = 0
-    for iteration, test_img in enumerate(all_data):
-        img_id = test_img['watchlink']
-        loc = img_data_mappings[img_id]
-        lat, lon, var, delta_squared = calc_update(img_id, loc, G)
-        new_img_data_mappings[img_id] = Location(lat, lon, var)
-        mean_squared_change = mean_squared_change / (iteration+1) * iteration + (delta_squared / (iteration + 1))
-    img_data_mappings = new_img_data_mappings
-img = 'http://flickr.com/photos/51035718466@N01/2536809519'
-'''
-for neighbor in G.neighbors(img):
-    print(neighbor + ': ' + repr(img_data_mappings[neighbor]))
-    for test_img in all_data:
-        if neighbor == test_img['watchlink']:
-            print(test_img['keywords'].split(', '))
-            break
-
-print(img + ': ' + repr(img_data_mappings[img]))
-for test_img in test_data + train_data:
-    if img == test_img['watchlink']:
-        print(test_img['keywords'].split(', '))
-        break
-
-
-print()
-print()
-'''
-print('Num iterations: {0}'.format(num_iter))
-print('Converged?: {0}'.format(has_converged))
-# Calculate error
-median_error_finder = MedianFinder()
-one_km_count = 0
-five_km_count = 0
-ten_km_count = 0
-hundred_km_count = 0
-thousand_km_count = 0
-other_count = 0
-for test_img in test_data:
-    img_id = test_img['watchlink']
-    img_result_loc = img_data_mappings[img_id]
-    img_actual_loc = Location(float(test_img['latitude']), float(test_img['longitude']))
-    error = Location.dist(img_result_loc, img_actual_loc)
-    median_error_finder.add(error)
-    if error < 1:
-        one_km_count += 1
-        print(img_id + ': ' + repr(img_result_loc))
-    elif error < 5:
-        five_km_count += 1
-    elif error < 10:
-        ten_km_count += 1
-    elif error < 100:
-        hundred_km_count += 1
-    elif error < 1000:
-        thousand_km_count += 1
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--maxiter', nargs=1, type=int,
+            help='Max number of iterations to run.')
+    arguments = parser.parse_args() # pylint: disable=invalid-name
+    if arguments.maxiter is None:
+        main()
     else:
-        other_count += 1
-print('Less than 1 km: {0}'.format(one_km_count))
-print('Less than 5 km: {0}'.format(five_km_count))
-print('Less than 10 km: {0}'.format(ten_km_count))
-print('Less than 100 km: {0}'.format(hundred_km_count))
-print('Less than 1000 km: {0}'.format(thousand_km_count))
-print('Greater than 1000 km: {0}'.format(other_count))
-print('Med error: {0}'.format(median_error_finder.med()))
+        main(arguments.maxiter[0])
 
